5d73d56abaf5d14387533f185d244198cc5e3bea
Abdulse <88330043+Abdulse@users.noreply.github.com>
2021-09-02 13:34:45 +0300

    0-more-nested

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,26 +1,12 @@
-#include "holberton.h"
-#include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _isupper - checks for uppercase.
+ * @c: character to check.
+ * Return: 1 if c is uppercase, otherwise return 0.
  */
-
-/**
- * _isupper - check if character is upper case
- * @c : integer value
- * Return: 1 if c is upper case 0 otherwise
- *
- */
-
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
 	else
 		return (0);
 }
